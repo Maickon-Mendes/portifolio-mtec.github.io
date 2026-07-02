83bf90a9921920a791d94834bb16fad8d8ec49d6
MAICKON MENDES <maickonmmendes@gmail.com>
2026-07-02 02:52:58 -0300

    dia 1 de produção

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+const header = document.querySelector(".header-bg");
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
+    });
